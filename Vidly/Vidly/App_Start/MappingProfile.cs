41cdc072f2ec7f0016eab7e5ed853021c71c2f07
Hugo Morales Arjón <hugo_morarj@hotmail.com>
2016-08-14 22:14:05 -0500

    Update Vidly to Section 6 Class 72 Exercise

---
+++ Vidly/Vidly/App_Start/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Customer, CustomerDto>();
+            CreateMap<Movie, MovieDto>();
+
+            CreateMap<CustomerDto, Customer>().ForMember(c => c.Id, opt => opt.Ignore());
+            CreateMap<MovieDto, Movie>().ForMember(c => c.Id, opt => opt.Ignore());
+        }  
+    }
+}
