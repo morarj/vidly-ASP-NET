394f8e32ad017811a827bcd5433242a0f2a89993
Hugo Morales Arjón <hugo_morarj@hotmail.com>
2016-09-14 19:31:17 -0500

    Update Vidly to the last lesson

--- Vidly/Vidly/App_Start/Startup.Auth.cs
+++ Vidly/Vidly/App_Start/Startup.Auth.cs
@@ -6,6 +6,7 @@ using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
 using Vidly.Models;
+using System.Configuration;
 
 namespace Vidly
 {
@@ -55,8 +56,8 @@ namespace Vidly
             //   consumerSecret: "");
 
             app.UseFacebookAuthentication(
-               appId: "123",
-               appSecret: "abc");
+               appId: ConfigurationManager.AppSettings["FacebookId"],
+               appSecret: ConfigurationManager.AppSettings["FacebookAppSecret"]);
 
             //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
             //{
